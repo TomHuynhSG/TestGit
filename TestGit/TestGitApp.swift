0c17ac5f03cc69750a3c421635779169e4456142
Tom Huynh <tom.huynh@rmit.edu.vn>
2024-07-22 08:45:10 +0700

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Tom Huynh on 22/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
